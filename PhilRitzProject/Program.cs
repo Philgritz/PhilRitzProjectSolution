3b9d617cf5d3e45db6c1a3887474309c03b06024
max-student@outlook.com <max-student@outlook.com>
2019-08-27 09:21:37 -0400

    aaa

---
+++ PhilRitzProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PhilRitzProject {
+    class Program {
+        static void Main(string[] args) 
+            {
+
+            string FirstNameLastName = "";
+            FirstNameLastName = "Phil Ritz";
+            Console.WriteLine(FirstNameLastName);
+
+            string Address = "";
+            Address = "2507 Moorman Ave";
+           Console.WriteLine(Address);
+
+            string City = "";
+            City = "Cincinnati";
+
+            string State = "";
+            State = "Ohio";
+
+            string Zip = "";
+            Zip = "45206";
+
+            Console.WriteLine(City + " " + State + " " + Zip);
+
+            string Phone = "";
+            Phone = "513-255-6477";
+            Console.WriteLine(Phone);
+
+            string Email = "";
+            Email = "Philgritz@gmail.com";
+            Console.WriteLine(Email);
+
+
+        }
+    }
+}
